f62e9c90d1fd573eb8c39b92a7d8810a025abef1
Aleksander Vines <aleksandervines1@hotmail.com>
2015-09-24 10:52:26 +0200

    Executes setup script  if running up on windows

--- setup.bat
+++ setup.bat
@@ -1,15 +1,18 @@
 @echo off
 
+echo "Started the setup script."
 REM If you used the stand Cygwin installer this will be C:\cygwin
 set CYGWIN=C:\programmer\cygwin64
 
 REM You can switch this to work with bash with %CYGWIN%\bin\bash.exe
 set SH=%CYGWIN%\bin\bash.exe
-set VAGRANT_FOLDER=/cygdrive/c/Users/alevin/Documents/Project/NORMAP/ansible/nersc-vagrant
+set CYGWIN_VAGRANT_FOLDER=/cygdrive/c/Users/alevin/Documents/Project/NORMAP/ansible/nersc-vagrant
 
-"%SH%" -c "/usr/bin/chmod -x %VAGRANT_FOLDER%/provisioning/hosts"
-"%SH%" -c "/usr/bin/sed -i 's/#\[ssh_connection]/\[ssh_connection]/g' %VAGRANT_FOLDER%/ansible.cfg"
-"%SH%" -c "/usr/bin/sed -i 's/#control_path/control_path/g' %VAGRANT_FOLDER%/ansible.cfg"
-"%SH%" -c "/usr/bin/sed -i 's/testconda.vm.synced_folder/#testconda.vm.synced_folder/g' %VAGRANT_FOLDER%/Vagrantfile"
+"%SH%" -c "/usr/bin/chmod -x %CYGWIN_VAGRANT_FOLDER%/provisioning/hosts"
+"%SH%" -c "/usr/bin/sed -i 's/#\[ssh_connection]/\[ssh_connection]/g' %CYGWIN_VAGRANT_FOLDER%/ansible.cfg"
+"%SH%" -c "/usr/bin/sed -i 's/#control_path/control_path/g' %CYGWIN_VAGRANT_FOLDER%/ansible.cfg"
+REM "%SH%" -c "/usr/bin/sed -i 's/testconda.vm.synced_folder/#testconda.vm.synced_folder/g' %CYGWIN_VAGRANT_FOLDER%/Vagrantfile"
 
-PAUSE
+echo "Finished the setup script."
+
+REM PAUSE
