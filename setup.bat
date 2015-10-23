918048d974bfa5f19c738e20a444f59b772b5501
Aleksander Vines <aleksandervines1@hotmail.com>
2015-10-23 18:50:04 +0200

    CYGWIN_VAGRANT_FOLDER made dynamic

--- setup.bat
+++ setup.bat
@@ -6,7 +6,8 @@ set CYGWIN=C:\programmer\cygwin64
 
 REM You can switch this to work with bash with %CYGWIN%\bin\bash.exe
 set SH=%CYGWIN%\bin\bash.exe
-set CYGWIN_VAGRANT_FOLDER=/cygdrive/c/Users/alevin/Documents/Project/NORMAP/ansible/nersc-vagrant
+set cmd="pwd"
+FOR /F %i IN (' %cmd% ') DO SET CYGWIN_VAGRANT_FOLDER=%i
 
 "%SH%" -c "/usr/bin/chmod -x %CYGWIN_VAGRANT_FOLDER%/provisioning/hosts"
 "%SH%" -c "/usr/bin/sed -i 's/#\[ssh_connection]/\[ssh_connection]/g' %CYGWIN_VAGRANT_FOLDER%/ansible.cfg"
