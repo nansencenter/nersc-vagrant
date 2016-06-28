77ca2f55b395e98d5cd1ef0cc66ff1eaff1043c2
Aleksander Vines <Aleksander.Vines@nersc.no>
2016-06-28 12:25:26 +0200

    modified default cygwin location

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
-
+echo RUNNING SCRIPT
 REM If you used the stand Cygwin installer this will be C:\cygwin
-set CYGWIN=C:\programmer\cygwin64
+set CYGWIN=C:\programmes\cygwin64
 
 REM You can switch this to work with bash with %CYGWIN%\bin\bash.exe
 set SH=%CYGWIN%\bin\bash.exe
